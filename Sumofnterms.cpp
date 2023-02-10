81997a1733eaa0bad878f0ed7ca6f413443f138d
JATIN <111575065+Jatin14052001@users.noreply.github.com>
2023-02-10 13:37:56 +0530

    Sum of nth terms code

---
+++ Sumofnterms.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+//Sum of n terms of an AP is Sum=n*(n+1)/2
+
+int main(){
+
+    int n,sum;
+
+    cout<<"Enter value of n is : ";
+    cin>>n;
+
+    sum=n*(n+1)/2;
+
+    cout<<"Sum of n terms is : "<<sum<<endl;
+    return 0;
+}
+
