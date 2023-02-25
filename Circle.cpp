8cfb4e04bdd2e864365237acf92569207859b787
JATIN <111575065+Jatin14052001@users.noreply.github.com>
2023-02-25 19:31:04 +0530

    Finding_Area_of_a_circle

---
+++ Circle.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+//Printing Area of a circle = 3.14*radius*radius
+int main(){
+
+    float r;
+    int Area;
+
+    cout<<"The value of radius is : ";
+    cin>>r;
+
+    Area=3.14*r*r;
+
+    cout<<"Area of a circle is : "<<Area<<endl;
+    return 0;
+}
