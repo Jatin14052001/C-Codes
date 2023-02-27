2cdec9c6732561588dd73f97c45d7dddae0dd946
JATIN <111575065+Jatin14052001@users.noreply.github.com>
2023-02-27 18:59:08 +0530

    Using_Bitwise_Operator(Or/And)

---
+++ Bitwise.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+//Using Bitwise Operator printing values 
+int main(){
+    int x=11,y=5,z,w;
+
+    z=x&y;
+    w=x|y;
+    cout<<"The value of z is : "<<z<<endl;
+    cout<<"The value of w is : "<<w<<endl;
+    return 0;
+
+
+
+}
